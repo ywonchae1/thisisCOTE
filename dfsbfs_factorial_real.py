33f6d4cd86a2ac2a434a660a0674b961b9c007f7
양원채 <20201000@sungshin.ac.kr>
2023-03-27 22:15:44 +0900

    8th commit

--- dfsbfs_factorial_real.py
+++ dfsbfs_factorial_real.py
@@ -13,7 +13,7 @@ def factorial_recursive(n):
     if n <= 1:      #n이 1 이하인 경우 n을 반환
         return n
     #n! = n * (n - 1)!를 그대로 코드로 작성하기
-    return n * factorial_iterative(n - 1)
+    return n * factorial_recursive(n - 1)
 
 #각각의 방식으로 구현한 n! 출력(n = 5)
 print('반복적으로 구현:', factorial_iterative(5))
